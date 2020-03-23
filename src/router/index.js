6441f2a7c572895fcd8a56d5a6377ec9c3c84b42
chenjiale123 <1756692091@qq.com>
2020-03-23 16:39:17 +0800

    c

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login'
+import Home from '../components/Home'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
+import Rights from '../components/power/Rights'
+import Roles from '../components/power/Roles'
+
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [{
+      path: '/welcome', component: Welcome
+    }, {
+      path: '/users', component: Users
+    }, {
+      path: '/rights', component: Rights
+    }, {
+      path: '/roles', component: Roles
+    }]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
